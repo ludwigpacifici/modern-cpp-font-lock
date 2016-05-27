4cf4a8e77691aae93745cbf0f04afca9d1ec2fd1
Ludwig PACIFICI <ludwig@lud.cc>
2016-05-27 22:58:58 +0100

    Better preview

--- preview.cc
+++ preview.cc
@@ -1,15 +1,11 @@
 #pragma once
 
-#define CONCATENATE_IMPL(s1, s2) s1##s2
-#define CONCATENATE(s1, s2) CONCATENATE_IMPL(s1, s2)
-#define ANONYMOUS_VARIABLE(str) CONCATENATE(str, __LINE__)
+#define EXPAND_ME __FILE__ " : " __LINE__
 
-constexpr int pi = 314LLu;
+constexpr auto pi = 314LLu;
 thread_local decltype(pi) rage = 0b10;
 
 [[deprecated("abc")]] char16_t *f() noexcept { return nullptr; }
-
-static_assert(std::is_copy_constructible<T>::value, "Swap requires copying");
 
 template <typename T> struct Foo {
   static_assert(std::is_floating_point<T>::value,
