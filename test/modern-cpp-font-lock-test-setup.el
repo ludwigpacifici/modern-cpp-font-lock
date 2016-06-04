d9b625482b82de118ab23a6acd1dbdec87c425c7
Ludwig PACIFICI <ludwig@lud.cc>
2016-06-04 09:23:57 +0100

    Added Automated tests via faceup

---
+++ test/modern-cpp-font-lock-test-setup.el
@@ -0,0 +1,37 @@
+;;; modern-cpp-font-lock-test-setup.el --- Setup and execute all tests
+
+;;; Commentary:
+
+;; This package sets up a suitable enviroment for testing
+;; modern-cpp-font-lock, and executes the tests.
+;;
+;; Usage:
+;;
+;;   emacs -q -l modern-cpp-font-lock-test-setup.el
+;;
+;; Note that this package assumes that some packages are located in
+;; specific locations.
+
+;;; Code:
+
+(setq inhibit-startup-screen t)
+(prefer-coding-system 'utf-8)
+
+(defvar modern-cpp-font-lock-test-setup-directory
+  (if load-file-name
+      (file-name-directory load-file-name)
+    default-directory))
+
+(dolist (dir '("." ".." "faceup"))
+  (add-to-list 'load-path
+               (concat modern-cpp-font-lock-test-setup-directory dir)))
+
+(require 'cc-mode)
+(require 'modern-cpp-font-lock)
+(add-hook 'c++-mode-hook #'modern-c++-font-lock-mode)
+
+(require 'modern-cpp-font-lock-test)
+
+(ert t)
+
+;;; modern-cpp-font-lock-test-setup.el ends here
---
+++ test/modern-cpp-font-lock-test.el
@@ -0,0 +1,19 @@
+(require 'faceup)
+
+(defvar modern-cpp-font-lock-test-dir (faceup-this-file-directory))
+
+(defun modern-cpp-font-lock-test-facit (dir)
+  "Test that `dir'/test.cc is fontifies as the .faceup file describes.
+`dir' is interpreted as relative to this source directory."
+  (faceup-test-font-lock-file 'c++-mode
+                              (concat
+                               modern-cpp-font-lock-test-dir
+                               dir
+                               "/test.cc")))
+
+(faceup-defexplainer modern-cpp-font-lock-test-facit)
+
+(ert-deftest modern-cpp-font-lock-file-test ()
+  (should (modern-cpp-font-lock-test-facit "cpp/preview")))
+
+(provide 'modern-cpp-font-lock-test)
